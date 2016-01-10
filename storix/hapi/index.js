a72ddc002c10a17aa6c82b5f9234c567be2883c9
Bambang Purnomosidi D. P <bambangpdp@yahoo.com>
2016-01-10 13:36:51 +0700

    Based on hapi-rethinkdb, almost works!

--- storix/hapi/index.js
+++ storix/hapi/index.js
@@ -2,7 +2,21 @@
 
 const Hapi = require('hapi');
 const server = new Hapi.Server();
+/*
 var Storix = require('storix');
+const storix = new Storix();
+
+console.log(storix.getProduct(1));
+*/
+
+server.register({
+  register: require('storix'),
+  opts: { dbName: '_system' }
+//  register: require('storix'),
+//  opts: { url: 'rethinkdb://:password@domain.tld:port/dbname' }
+}, function (err) {
+  if (err) console.error(err);
+});
 
 server.connection({ port: 3000 });
 
@@ -10,15 +24,16 @@ server.route({
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
-    var string 
-    reply('Hello, this is "storix" show case.<br />' +
-      'I live only to test the plugin');
+    //var string 
+    // reply('Hello, this is "storix" show case.<br />' +
+    //  'I live only to test the plugin');
+
+    var conn = request.server.plugins['storix'].connection;
+    reply(conn);
   }
 });
 
-const storix = new Storix();
 
-console.log(storix.getProduct(1));
 
 server.start((err) => {
     if (err) {
--- storix/plugin/lib/index.js
+++ storix/plugin/lib/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const internals = {};
 
 exports = module.exports = internals.Storix = function() {
@@ -25,3 +26,62 @@ var products = [{
     name: 'Banjo'
   }
 ];
+*/
+
+var arango = require('arangojs')();
+
+exports.register = function (plugin, opts, next) {
+  opts = opts || {}
+  /*
+  if (!opts.url) {
+    opts.port = opts.port || 8529
+    opts.host = opts.host || 'localhost'
+    opts.db = opts.db || '_system'
+  } else {
+    var url = require('url').parse(opts.url)
+    opts.port = parseInt(url.port, 10) || 8529
+    opts.host = url.hostname || 'localhost'
+    opts.db = url.pathname ? url.pathname.replace(/^\//, '') : '_system'
+
+    if (url.auth)
+      opts.authKey = url.auth.split(':')[1]
+  }
+  
+  arango.useDatabase(opts, function (err, conn) {
+    if (err) {
+      plugin.log(['storix', 'error'], err.message)
+      console.error(err)
+      return next(err)
+    }
+
+    plugin.expose('connection', conn)
+    plugin.expose('library', arango)
+    plugin.expose('arango', arango)
+    plugin.bind({
+      arangodbConn: conn,
+      arangodb: arango
+    })
+   */
+/*
+  arango.useDatabase(opts.dbName, function (err, conn) {
+    if (err) {
+      plugin.log(['storix', 'error'], err.message)
+      console.error(err)
+      return next(err)
+    }
+*/
+    arango.useDatabase(opts.dbName);
+    plugin.expose('connection', arango)
+
+    plugin.log(['storix', 'info'], 'ArangoDB connection established')
+    /*
+  });
+*/
+  return next()
+
+}
+
+exports.register.attributes = {
+  pkg: require('../package.json')
+}
+
